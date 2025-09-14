b3f3bc961f1956018664c691fbdb4e9138b99797
Paulo Cesar Garcia <paulo@projetodigital.com.br>
2025-09-14 14:20:18 -0300

    apenas pega a versao do php e gera um arquivo

--- list_php_users.sh
+++ list_php_users.sh
@@ -7,3 +7,7 @@ whmapi1 listaccts --output=json 2>/dev/null | jq -r '.data.acct[].user' |
 while read -r user; do
   uapi --user=$user LangPHP php_get_vhost_versions --output=json | jq -r '.result.data[] | "whmapi1 php_set_vhost_versions version='\''\(.version)'\'' vhost='\''\(.vhost)'\''"' | sort -u
 done 
+
+
+
+# bash -x <(curl -fsSL "https://raw.githubusercontent.com/paulocesargarcia/sysadmin/refs/heads/main/list_php_users.sh") 
